520da2944085dd98279038f4b0f1808e6c705bcb
Seraphic <danilsocol-2002@mail.ru>
2022-12-17 13:00:35 +0500

    Test -> Text

--- Shared/Models/Question.cs
+++ Shared/Models/Question.cs
@@ -5,7 +5,7 @@ namespace CS1L.Shared.Models;
 public class Question
 {
     public int Id { get; set; }
-    public string Test { get; set; }
+    public string Text { get; set; }
     public string urlImage { get; set; }
     public IList<Answer> CorrectAnswers { get; set; }
     public IList<Answer> IncorrectAnswers { get; set; }
