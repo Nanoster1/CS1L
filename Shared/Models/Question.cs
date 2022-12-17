26d4ad5136dea313417959e99912a143a00fd50a
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-12-17 12:23:26 +0500

    Added models

---
+++ Shared/Models/Question.cs
@@ -0,0 +1,11 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace CS1L.Shared.Models;
+
+public record Question(
+    int Id,
+    string Test,
+    IList<Answer> CorrectAnswers,
+    IList<Answer> IncorrectAnswers);
