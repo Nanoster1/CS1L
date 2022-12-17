2a5de41c139c55a92497636643c84764bf755ff0
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-12-17 19:07:53 +0500

    Danil pidor

--- CS1L.Server/Controllers/SessionController.cs
+++ CS1L.Server/Controllers/SessionController.cs
@@ -1,4 +1,8 @@
-﻿using CS1L.Core.Sessions.Services;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CS1L.Core.Sessions.Services;
 using CS1L.Server.Controllers.Common;
 using CS1L.Server.Data;
 using CS1L.Shared.Models;
