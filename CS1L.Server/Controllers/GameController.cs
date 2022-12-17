166ca12e82549e64f78b5d205b25c78fe0b8d0d0
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-12-17 17:14:59 +0500

    Fixed folder structure

---
+++ CS1L.Server/Controllers/GameController.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CS1L.Server.Controllers;
+
+[ApiController]
+[Route("game")]
+public class GameController : Controller
+{
+    [HttpPost("connect/{id}")]
+    public async ValueTask Connect([FromRoute] Guid id)
+    {
+
+    }
+}
