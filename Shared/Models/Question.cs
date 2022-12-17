fa85096564c80ced8de6213791dc4d2b8cc068e1
Seraphic <danilsocol-2002@mail.ru>
2022-12-17 13:08:26 +0500

    AllAnswer

--- Shared/Models/Question.cs
+++ Shared/Models/Question.cs
@@ -7,7 +7,7 @@ public class Question
     public int Id { get; set; }
     public string Text { get; set; }
     public string urlImage { get; set; }
-    public IList<Answer> CorrectAnswers { get; set; }
-    public IList<Answer> IncorrectAnswers { get; set; }
+    public IList<Answer> CorrectAnswers { get; set; } = new List<Answer>();
+    public IList<Answer> AllAnswers { get; set; } = new List<Answer>();
 }
    
