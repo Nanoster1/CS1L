5b149e9f1af330d84d610a9ac4be3a7caaa92918
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-12-17 13:02:04 +0500

    Added EntityFramework and Migrations

---
+++ Server/Data/TestsContext.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using CS1L.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CS1L.Server.Data;
+
+public class TestsContext : DbContext
+{
+    public DbSet<Test> Tests { get; set; } = null!;
+
+    public TestsContext(DbContextOptions<TestsContext> options)
+            : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+
+}
