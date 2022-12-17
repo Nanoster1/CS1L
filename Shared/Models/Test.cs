45bb59e164729be40d6e3d13bf6f6c1fc16eac64
Seraphic <danilsocol-2002@mail.ru>
2022-12-17 12:55:28 +0500

    Test -> class

--- Shared/Models/Test.cs
+++ Shared/Models/Test.cs
@@ -1,10 +1,10 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
+﻿
 namespace CS1L.Shared.Models;
 
-public record Test(
-    int Id,
-    string Name,
-    IList<Question> Questions);
+public class Test
+{
+    private int Id { get; set; }
+    private string Name { get; set; }
+    private IList<Question> Questions { get; set; }
+    
+}
