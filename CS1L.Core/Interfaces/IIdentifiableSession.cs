3543576c38d1fcff4c84d2987c1ac35e6d83bd6c
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-12-17 17:07:41 +0500

    Extracted session Identity to an interface

--- CS1L.Core/Interfaces/IIdentifiableSession.cs
+++ CS1L.Core/Interfaces/IIdentifiableSession.cs
@@ -2,11 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using CS1L.Core.Interfaces;
+namespace CS1L.Core.Interfaces;
 
-namespace CS1L.Core.Sessions.Models;
-
-public class HostSession : ISessionIdentity
+public interface ISessionIdentity
 {
     public Guid Id { get; set; }
     public ulong VkId { get; set; }
